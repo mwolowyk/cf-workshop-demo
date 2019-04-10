089da6974bf8bbea2d65d133c5e9454a6d9e282d
mwolowyk <michael.wolowyk@springernature.com>
2019-04-10 11:45:23 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping
+public class DemoController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
