16cd87aea4111993145d5fc0f69586692be2b377
mwolowyk <michael.wolowyk@springernature.com>
2019-04-10 14:49:59 +0200

    add secret

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,18 +1,21 @@
 package com.example.demo;
 
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
 public class DemoController {
 
+    @Value("${my_secret}")
+    String secret;
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("secret", secret);
         return "index";
     }
 }
